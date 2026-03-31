5f51f00bd0dc635e0c2a00d8b70c7e4b866aa679
DESKTOP-JOEUJIC\johan <johanna.forsner.info@gmail.com>
2026-03-31 02:20:54 +0200

    Minor stockfish wrapper fix

--- engine/stockfish-wrapper.js
+++ engine/stockfish-wrapper.js
@@ -1,5 +1,5 @@
 async function init() {
-  const resp = await fetch('engine/stockfish-18-single.wasm.gz');
+  const resp = await fetch('stockfish-18-single.wasm.gz');
   const stream = resp.body.pipeThrough(new DecompressionStream('gzip'));
   const buffer = await new Response(stream).arrayBuffer();
   const blob = new Blob([buffer], { type: 'application/wasm' });
