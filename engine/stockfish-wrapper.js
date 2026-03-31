6e4cfd4c14c7e30d1c81126f0f401a95f713e9bd
DESKTOP-JOEUJIC\johan <johanna.forsner.info@gmail.com>
2026-03-31 03:00:53 +0200

    Minor stockfish wrapper fix 5 ...

--- engine/stockfish-wrapper.js
+++ engine/stockfish-wrapper.js
@@ -1,12 +1,13 @@
+// Set locateFile synchronously before anything else runs
+self.locateFile = (file) => file.endsWith('.wasm') ? self._wasmBlobUrl : file;
+
 async function init() {
   const base = self.location.href.replace(/\/[^/]+$/, '/');
   const resp = await fetch(base + 'stockfish-18-single.wasm.gz');
   const stream = resp.body.pipeThrough(new DecompressionStream('gzip'));
   const buffer = await new Response(stream).arrayBuffer();
   const blob = new Blob([buffer], { type: 'application/wasm' });
-  const wasmUrl = URL.createObjectURL(blob);
-
-  self.locateFile = (file) => file.endsWith('.wasm') ? wasmUrl : file;
+  self._wasmBlobUrl = URL.createObjectURL(blob);
 
   importScripts('stockfish-18-single.js');
 }
